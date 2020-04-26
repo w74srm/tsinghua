30179ad3d4df159345095080881354ac8c296054
w74srm <1178203994@qq.com>
2020-04-26 19:07:12 +0800

    backup 2020/4/26

--- cppfile/zoj patest/1020.cpp
+++ cppfile/zoj patest/1020.cpp
@@ -1,17 +1,46 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 using namespace std;
-
-const int maxn = 35;
-int postorder[maxn],inorder[maxn];
-vector<int> levelorder;
-
-int searchinInorder(int start ,int end, int n){
-    for(int i=start;i<=end;i++){
-        if(inorder[i]==n) return i;
+ 
+int n;
+vector<int> level[31];
+int num[31]={0};
+int post[30]={0}, in[30]={0};
+ 
+ 
+void pre(int post1,int post2,int in1,int in2,int lev){
+    if(post1>post2||in1>in2||lev>n)
+        return;
+    level[lev].push_back(post[post2]);
+    num[lev]++;
+    int i;
+    for(i=in1;i<=in2;i++){
+        if(in[i]==post[post2])
+            break;
     }
+    pre(post1,post1+i-in1-1,in1,in1+i-in1-1,lev+1);
+    pre(post1+i-in1,post2-1,i+1,in2,lev+1);
 }
-
-void trans(int start,int end){
-    levelorder.push_back(postorder[end]);
+ 
+int main()
+{
+    int i;
+    cin>>n;
+ 
+    for(i=0;i<n;i++)
+        cin>>post[i];
+    for(i=0;i<n;i++)
+        cin>>in[i];
+ 
+    pre(0,n-1,0,n-1,1);
+ 
+    cout<<level[1][0];
+    for(i=2;i<31;i++){
+        for(int j=0;j<num[i];j++){
+            if(level[i][j]==0)
+                continue;
+            cout<<" "<<level[i][j];
+        }
+    }
+    return 0;
 }
