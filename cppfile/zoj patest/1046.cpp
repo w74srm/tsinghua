018b62c6a2bd5957477dfb105282ba37cdac28ef
w74srm <1178203994@qq.com>
2020-11-12 16:05:28 +0800

    backup 2020/11/12

---
+++ cppfile/zoj patest/1046.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<cstdio>
+#include<vector>
+#include<algorithm>
+using namespace std;
+ 
+int main()
+{
+	int N; vector<int> v;
+	cin >> N;
+	int d = 0; int sum = 0;
+	v.push_back(d);
+	int i = 0;
+	while (N--)
+	{
+		cin >> d;
+		sum += d;
+		v.push_back(d+v[i++]);
+	}
+	int M;
+	cin >> M;
+	int f, l;
+	while (M--)
+	{
+		cin >> f >> l;
+		cout << min(abs(v[f-1] - v[l-1]), sum-abs(v[f-1] - v[l-1])) << endl;
+	}
+}
